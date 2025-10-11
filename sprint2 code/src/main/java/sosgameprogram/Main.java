e51e5737370a715633d5b7de80e1d58abd275f26
gavinbarton7 <gavinbarton@gmail.com>
2025-10-11 13:58:07 -0500

    First commit on 10-11

---
+++ sprint2 code/src/main/java/sosgameprogram/Main.java
@@ -0,0 +1,14 @@
+package sosgameprogram;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+  public static void main(String[] args) {
+    // Creates frame for SOS GUI and sets the frame size
+    SosGuiFrame frame = new SosGuiFrame();
+    frame.setSize(400,400);
+  }
+
+}
