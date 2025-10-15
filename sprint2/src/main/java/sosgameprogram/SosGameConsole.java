2a411ca301c87934e28c5668fef4f213911a401a
gavinbarton7 <gavinbarton@gmail.com>
2025-10-15 15:57:10 -0500

    4th commit on 10-15

--- sprint2/src/main/java/sosgameprogram/SosGameConsole.java
+++ sprint2/src/main/java/sosgameprogram/SosGameConsole.java
@@ -7,11 +7,15 @@ public class SosGameConsole {
   String gameMode;
   // currentPlayer holds "B" for blue player or "R" for red player
   String currentPlayer;
+  String[][] gameBoard;
+  boolean gameCurrentlyPlaying;
+
 
   public SosGameConsole() {
     this.boardSize = 8;
     this.gameMode = "S";
     this.currentPlayer = "B";
+    this.gameCurrentlyPlaying = false;
   }
 
   // Sets the size of the board
@@ -23,11 +27,25 @@ public class SosGameConsole {
     return boardSize;
   }
 
+
   public void setGameMode(String modeValue) {
     this.gameMode = modeValue;
   }
 
   public String getGameMode() {
     return gameMode;
+  }
+
+  // Sets up an empty board of the selected board size and sets the current player to the
+  // blue player since the blue player moves first in every game
+  public void setUpForNewGame() {
+    gameBoard = new String[boardSize][boardSize];
+    for (int i = 0; i < boardSize; i++) {
+      for (int j = 0; j < boardSize; j++) {
+        gameBoard[i][j] = "";
+      }
+    }
+    currentPlayer = "B";
+    gameCurrentlyPlaying = true;
   }
 }
