a6ab6fd9e12f97f014a344eb88e47f5ee7ae1125
gavinbarton7 <gavinbarton@gmail.com>
2025-12-04 09:27:19 -0600

    2nd commit on 12-4

--- sprint5/src/main/java/sosgameprogram/SosGameRecorder.java
+++ sprint5/src/main/java/sosgameprogram/SosGameRecorder.java
@@ -1,5 +1,8 @@
 package sosgameprogram;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,5 +35,64 @@ public class SosGameRecorder {
       String move = player + "," + row + "," + column + "," + letter;
       movesInRecordedGame.add(move);
     }
+  }
+
+  public boolean saveGameRecordingToOutputFile(String fileName) {
+
+    if (movesInRecordedGame.isEmpty()) {
+      return false;
+    }
+
+    BufferedWriter writer = null;
+    try {
+      writer = new BufferedWriter(new FileWriter(fileName));
+
+      // Writes all of the pre-game configurations to the file
+      writer.write("Game Mode:" + gameMode);
+      writer.newLine();
+      writer.write("Board Size:" + boardSize);
+      writer.newLine();
+      writer.write("Blue Player Type:" + bluePlayerType);
+      writer.newLine();
+      writer.write("Red Player Type:" + redPlayerType);
+      writer.newLine();
+
+
+      // This (and following for loop) writes all of the moves made in the game to the file
+      writer.write("Moves Made in the Game:");
+      writer.newLine();
+
+      for (String move : movesInRecordedGame) {
+        writer.write(move);
+        writer.newLine();
+      }
+
+      writer.flush();
+      return true;
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    } finally {
+      if (writer != null) {
+        try {
+          writer.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  public void stopGameRecording() {
+    this.isRecordingGame = false;
+  }
+
+  public boolean isGameCurrentlyRecording() {
+    return isRecordingGame;
+  }
+
+  public int getNumberOfMoves() {
+    return movesInRecordedGame.size();
   }
 }
